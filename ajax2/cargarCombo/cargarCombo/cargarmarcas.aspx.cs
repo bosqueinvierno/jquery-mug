e8358f629c7b7af58790ad615858d78c897e665f
Leonardo Micheloni <leonardo@kinetica-solutions.com>
2013-09-23 20:00:52 -0300

    cargar combo ajax

--- ajax2/cargarCombo/cargarCombo/cargarmarcas.aspx.cs
+++ ajax2/cargarCombo/cargarCombo/cargarmarcas.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +12,20 @@ namespace cargarCombo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var marcas = new List<Marca>();
+            marcas.Add(new Marca { Id = "ford", nombre = "ford" });
+            marcas.Add(new Marca { Id = "renault", nombre = "renault" });
+            marcas.Add(new Marca { Id = "citroen", nombre = "citroen" });
+            marcas.Add(new Marca { Id = "peugeot", nombre = "peugeot" });
 
+            Response.ContentType = "application/json";
+            Response.Write(new JavaScriptSerializer().Serialize(marcas));
         }
+    }
+
+    public class Marca
+    {
+        public string Id { get; set; }
+        public string nombre { get; set; }
     }
 }
